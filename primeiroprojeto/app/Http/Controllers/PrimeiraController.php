9deb384e7e77c396d7e2c402ed7aef4ccd02b07c
noemi-santos <149540792+noemi-santos@users.noreply.github.com>
2025-08-26 21:40:00 -0300

    Aula-26/08

--- primeiroprojeto/app/Http/Controllers/PrimeiraController.php
+++ primeiroprojeto/app/Http/Controllers/PrimeiraController.php
@@ -20,4 +20,15 @@ class PrimeiraController extends Controller
         $soma = $valor1 + $valor2;
         return "Soma: ".$soma;
     }
+
+    public function exercicio2(){
+        return view("exercicio2");
+    }
+
+     public function respExercicio2(Request $request){
+        $valor1 = $request->valor1;
+        $valor2 = $request->valor2;
+        $subtração = $valor1 - $valor2;
+        return "Subtração: ".$subtração;
+    }
 }
