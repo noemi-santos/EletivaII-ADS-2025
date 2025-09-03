c1ecd635789b5e05f56a8ddbc5757a015f88eb39
noemi-santos <149540792+noemi-santos@users.noreply.github.com>
2025-09-02 23:11:02 -0300

    aula_02/09

--- primeiroprojeto/routes/web.php
+++ primeiroprojeto/routes/web.php
@@ -2,6 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PrimeiraController;
+use App\Http\Controllers\ClienteController;
+
+Route::resource('clientes', ClienteController::class);
+//Listar Clientes - GET /clientes
+//Abrir formulário para inserir registro - GET/clientes/create
+//Salvar dados - POST /clientes -- método store
+//Mostrar dados do registro - GET /cleintes/ {id_clientes} -- método show
+//Abrir formulário para editar registro - GET /clientes/{id_clientes}/edit -- método edit
+//Salvar alterações - PUT /clientes/{id_clientes} -- método update
+//Excluir um registro - DELETE /clientes/{id_cliente} -- método destroy
+
+
+
+
+
+
+
+
+//Route::get("/clientes", function() {return view("clientes.index");});
 
 Route::get('/teste', [PrimeiraController::class, "teste"]);
 
